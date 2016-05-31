ee981b3e945970940ddfba3321d1ee6b8a8f7f9f
Managers RHA <mngrha@chronos.extia.fr>
2016-05-31 17:16:06 +0200

    gestion des routes et vue du formulaire login/register/logout

---
+++ app/scripts/controllers/AuthenticationController.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projet7AlbumManagerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the projet7AlbumManagerApp
+ */
+angular.module('projet7AlbumManagerApp')
+  .controller('AuthenticationController', function ($scope, $location, namedRouteService) {
+    
+  	$scope.login = function(){
+  		alert("login");
+  	}
+
+  	$scope.register = function(){
+  		alert("register");
+  	}
+
+  	$scope.logout = function(){
+  		alert("logout");
+
+  		//Session.clear();
+    	$location.path(namedRouteService.reverse('login'));
+
+
+  	}
+
+	$scope.go = function ( path ) {
+		$location.path(namedRouteService.reverse(path));
+	};
+
+  });
