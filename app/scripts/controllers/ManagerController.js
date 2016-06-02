bb7e8c9d5ad49d68d40578f0038ebf84bee8a687
Azure Hawkwood <bladea25@gmail.com>
2016-06-02 16:43:38 +0200

    Manager fonctionnel avec donnees chargees de maniere statiques

--- app/scripts/controllers/ManagerController.js
+++ app/scripts/controllers/ManagerController.js
@@ -15,12 +15,7 @@ angular.module('projet7AlbumManagerApp')
 	$scope.longText = "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker";
 	$scope.maxLengthStringToDisplay = 240;
 
-	/*
-	$scope.init = function (){
-		//angular.element(document).ready(function () { ...	});
-	};
-	*/
-	$scope.initManagerJQEvents = initManagerJQEvents;
+	
 	$scope.initManager = initManager;
 
 
@@ -30,101 +25,12 @@ angular.module('projet7AlbumManagerApp')
 	$scope.actions = [];
 
 
-/*
-
-	$scope.init = function (){
-		angular.element(document).ready(function () { 
-			$(document).ready(function(){
-alert("huhu");
-				$('#managerContainerTable').dragtable({
-					dragHandle:'.move-horizontal',			//point de clic où l'on autorise le drag
-					dragaccept:'.move-horizontal-accept',	//colonnes acceptées dans le drag
-					clickDelay:0,	//délai à partir duquel le drag commence
-					distance:0,		//distance à partir de laquelle le drag commence	
-					revert: true,	//permet de faire l'anim de la colonne qui se replace bien
-					exact: false,	//IMPORTANT, ça casse le css sinon
-					containment: '#managerContainer',
-				 	beforeStart: function(){       
-				    },
-					beforeMoving: function(){       
-				    },
-					beforeReorganize: function(){       
-				    },
-					beforeStop: function(){       
-				    }
-
-				});
-			});
-		});
-	};
-
-
-
-
-$scope.init();
-*/
-
-
-
-
 	$scope.initManager();
-	$scope.initManagerJQEvents();
-
-/*
-$scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
-    //you also get the actual event object
-    //do stuff, execute functions -- whatever...
-    	alert("huhu");
-	$('#managerContainerTable').dragtable({
-			dragHandle:'.move-horizontal',			//point de clic où l'on autorise le drag
-			dragaccept:'.move-horizontal-accept',	//colonnes acceptées dans le drag
-			clickDelay:0,	//délai à partir duquel le drag commence
-			distance:0,		//distance à partir de laquelle le drag commence	
-			revert: true,	//permet de faire l'anim de la colonne qui se replace bien
-			exact: false,	//IMPORTANT, ça casse le css sinon
-			containment: '#managerContainer',
-		 	beforeStart: function(){       
-		    },
-			beforeMoving: function(){       
-		    },
-			beforeReorganize: function(){       
-		    },
-			beforeStop: function(){       
-		    }
-
-		});
-});
-*/
-
-	$scope.ngRepeatFinished = function(){
-		//alert("huhu");
-/*	$('#managerContainerTable').dragtable({
-			dragHandle:'.move-horizontal',			//point de clic où l'on autorise le drag
-			dragaccept:'.move-horizontal-accept',	//colonnes acceptées dans le drag
-			clickDelay:0,	//délai à partir duquel le drag commence
-			distance:0,		//distance à partir de laquelle le drag commence	
-			revert: true,	//permet de faire l'anim de la colonne qui se replace bien
-			exact: false,	//IMPORTANT, ça casse le css sinon
-			containment: '#managerContainer',
-		 	beforeStart: function(){       
-		    },
-			beforeMoving: function(){       
-		    },
-			beforeReorganize: function(){       
-		    },
-			beforeStop: function(){       
-		    }
-
-		});*/
-	};
-
-
+	
 
 	function initManager(){
 		
-
-
-	$scope.tasks = [
+		$scope.tasks = [
 			{	id: 1,
 				name: "task 1",
 				order: 1
@@ -148,7 +54,7 @@ $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
 		];
 
 
-	$scope.items = [
+		$scope.items = [
 			{	id: 1,
 				name: "item 1",
 				order: 1
@@ -169,18 +75,327 @@ $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
 				name: "item 5",
 				order: 5
 			}
-	];
-
-
-	$scope.actions = [];
-
-
-	}
-
-
-	function initManagerJQEvents(){
+		];
 
 		/*
+		$scope.actions = [
+			1: {
+
+
+
+			}, 2: {}, 3: {}];
+		*/
+
+		$scope.actions[1] = [];
+		$scope.actions[2] = [];
+		$scope.actions[3] = [];
+		$scope.actions[4] = [];
+		$scope.actions[5] = [];
+
+		$scope.actions[1][1] = {
+			id: 1,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[1][2] = {
+			id: 2,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[1][3] = {
+			id: 3,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "finished"
+		};
+		$scope.actions[1][4] = {
+			id: 4,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[1][5] = {
+			id: 5,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+
+
+		$scope.actions[2][1] = {
+			id: 6,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "started"
+		};
+		$scope.actions[2][2] = {
+			id: 7,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[2][3] = {
+			id: 8,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[2][4] = {
+			id: 9,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "tostart"
+		};
+		$scope.actions[2][5] = {
+			id: 10,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+
+		$scope.actions[3][1] = {
+			id: 11,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[3][2] = {
+			id: 12,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[3][3] = {
+			id: 13,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[3][4] = {
+			id: 14,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[3][5] = {
+			id: 15,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+
+		$scope.actions[4][1] = {
+			id: 16,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[4][2] = {
+			id: 17,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[4][3] = {
+			id: 18,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[4][4] = {
+			id: 19,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[4][5] = {
+			id: 20,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+
+		$scope.actions[5][1] = {
+			id: 21,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[5][2] = {
+			id: 22,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[5][3] = {
+			id: 23,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[5][4] = {
+			id: 24,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+		$scope.actions[5][5] = {
+			id: 25,
+			comment: "huhu commentaire",
+			userName: "mon user",
+			userLogin: "mon login",
+			dateLastModif: "06/05/2016",
+			stateClass: "undefined"
+		};
+
+
+	};
+
+});
+
+
+
+
+/*
+ * On va créer des directives qui serviront à lancer des événements d'une table draggable, on s'en sert
+ * pour les exécuter une fois les directives angular ng-repeat de la table terminées
+*/
+angular.module('projet7AlbumManagerApp').directive("waitOnColLoad", function(){
+    return {
+        restrict: 'A',
+        scope:{},	//Important pour pouvoir récupérer le scope de l'élément dans la fonction link
+        link: function(scope, element, attr){
+        	//Si c'est le dernier élément du ng-repeat que l'on charge
+        	if (scope.$parent.$last) {
+               	$('#managerContainerTable').dragtable({
+					dragHandle:'.move-horizontal',			//point de clic où l'on autorise le drag
+					dragaccept:'.move-horizontal-accept',	//colonnes acceptées dans le drag
+					clickDelay:0,	//délai à partir duquel le drag commence
+					distance:0,		//distance à partir de laquelle le drag commence	
+					revert: true,	//permet de faire l'anim de la colonne qui se replace bien
+					exact: false,	//IMPORTANT, ça casse le css sinon
+					containment: '#managerContainer',
+				 	beforeStart: function(){       
+				    },
+					beforeMoving: function(){       
+				    },
+					beforeReorganize: function(){       
+				    },
+					beforeStop: function(){       
+				    }
+				});
+            }
+        }
+    };
+});
+angular.module('projet7AlbumManagerApp').directive("waitOnRowLoad", function(){
+    return {
+        restrict: 'A',       
+        scope:{},	//Important pour pouvoir récupérer le scope de l'élément dans la fonction link
+        link: function(scope, element, attr){
+        	//Si c'est le dernier élément du ng-repeat que l'on charge
+        	if (scope.$parent.$last) {               
+				$("#managerContainerTable").rowSorter({
+					handler         : 'th .move-vertical',
+					stickTopRows    : 0,
+					stickBottomRows : 1,
+					onDragStart: function(tbody, row, index){
+				        //console.log('start index is ' + index);
+				    },
+				    onDrop: function(tbody, row, new_index, old_index){
+				        //console.log('sorted from ' + old_index + ' to ' + new_index);
+				    }
+				});
+            }
+        }
+    };
+});
+
+
+
+/*
+	Même lors d'un appel à la fonction dans le contrôleur suite à un : 
+	$scope.init = function (){
+		//angular.element(document).ready(function () { ...	});
+	};
+	cela s'exécute avant les directives ng-repeat
+	
+	function initManagerJQEvents(){
+	
+	
+		//Cela fonctionne pour le row sorter, je ne sais pas pourquoi
+		//même si le tableau n'a pas terminé de se construire à partir du ng-repeat,
+		//le rowSorter fonctionne quand même, probablement attache-il les événements
+		//sur la tableau et non directement sur les lignes td/tr ?
+		
+		$("#managerContainerTable").rowSorter({
+
+			handler         : 'th .move-vertical',
+			stickTopRows    : 1,
+			stickBottomRows : 1,
+			onDragStart: function(tbody, row, index){
+		        //console.log('start index is ' + index);
+		    },
+		    onDrop: function(tbody, row, new_index, old_index){
+		        //console.log('sorted from ' + old_index + ' to ' + new_index);
+		    }
+		});
+
+		//En revanche, le dragtable utilisé pour réordonner les colonnes ne marche pas, 
+		//avec angular, la page se charge, les événements sont lancés, et seulement ensuite,
+		//le ng-repeat tourne, ce qui fait que les colonnes ne sont pas encore chargées.
+		//A la place, on va créer des directives qui se lanceront uniquement lorsque les derniers
+		//éléments des ng-repeat seront insérés
 		$('#managerContainerTable').dragtable({
 			dragHandle:'.move-horizontal',			//point de clic où l'on autorise le drag
 			dragaccept:'.move-horizontal-accept',	//colonnes acceptées dans le drag
@@ -199,107 +414,5 @@ $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
 		    }
 
 		});
-*/
-
-/*
-
-		$("#managerContainerTable").rowSorter({
-
-			handler         : 'th .move-vertical',
-			stickTopRows    : 1,
-			stickBottomRows : 1,
-			onDragStart: function(tbody, row, index){
-		        //console.log('start index is ' + index);
-		    },
-		    onDrop: function(tbody, row, new_index, old_index){
-		        //console.log('sorted from ' + old_index + ' to ' + new_index);
-		    }
-		});
-
-
-*/
-
 	}
-
-
-
-});
-
-
-
-
-
-
-angular.module('projet7AlbumManagerApp').directive("waitOnRowLoad", function(){
-
-    return {
-
-        restrict: 'A',
-
-        scope:{},
-        link: function(scope, element, attr){
-        	
-        	if (scope.$parent.$last) {
-               
-               	$('#managerContainerTable').dragtable({
-					dragHandle:'.move-horizontal',			//point de clic où l'on autorise le drag
-					dragaccept:'.move-horizontal-accept',	//colonnes acceptées dans le drag
-					clickDelay:0,	//délai à partir duquel le drag commence
-					distance:0,		//distance à partir de laquelle le drag commence	
-					revert: true,	//permet de faire l'anim de la colonne qui se replace bien
-					exact: false,	//IMPORTANT, ça casse le css sinon
-					containment: '#managerContainer',
-				 	beforeStart: function(){       
-				    },
-					beforeMoving: function(){       
-				    },
-					beforeReorganize: function(){       
-				    },
-					beforeStop: function(){       
-				    }
-
-				});
-
-            }
-
-        }
-
-    };
-
-});
-
-
-
-
-angular.module('projet7AlbumManagerApp').directive("waitOnLineLoad", function(){
-
-    return {
-
-        restrict: 'A',
-
-       
-        scope:{},
-        link: function(scope, element, attr){
-        	
-        	if (scope.$parent.$last) {
-               
-				$("#managerContainerTable").rowSorter({
-
-					handler         : 'th .move-vertical',
-					stickTopRows    : 0,
-					stickBottomRows : 1,
-					onDragStart: function(tbody, row, index){
-				        //console.log('start index is ' + index);
-				    },
-				    onDrop: function(tbody, row, new_index, old_index){
-				        //console.log('sorted from ' + old_index + ' to ' + new_index);
-				    }
-				});
-
-            }
-		
-        }
-
-    };
-
-});
+*/
